f4a71772ec05e26428f9913c9d3fe30ac347e0cd
BRUNO DAL PRÁ <bruno_pra@estudante.sesisenai.org.br>
2025-04-29 12:11:49 -0300

    ?num=&num=

--- script2.js
+++ script2.js
@@ -4,23 +4,6 @@ const PORTA = 8000
 
 const server = express()
 
-server.get('/soma', () => {
-    const resultado = somar(1,2)
-    resultado.send(`Resultado da soma: ${resultado}`)
-})
-server.get('/subtracao', () => {
-    const resultado = subtracao(1,2)
-    resultado.send(`Resultado da subtracao: ${resultado}`)
-})
-server.get('/multiplicacao', () => {
-    const resultado = multiplicacao(1,2)
-    resultado.send(`Resultado da multiplicacao: ${resultado}`)
-})
-server.get('/divisao', () => {
-    const resultado = divisao(1,2)
-    resultado.send(`Resultado da divisao: ${resultado}`)
-})
-
 function soma (num1, num2){
     return num1+num2
 }
@@ -36,6 +19,28 @@ function multiplicacao(num1, num2){
 function divisao(num1,num2){
     return num1/num2
 }
+
+
+server.get('/soma', (req, res) => {
+    console.log(req.query)
+    const resultado = soma(Number(req.query.num1), Number(req.query.num2))
+    res.send(`Resultado da soma: ${resultado}`)
+})
+server.get('/subtracao', (req, res) => {
+    console.log(req.query)
+    const resultado = subtracao(Number(req.query.num1), Number(req.query.num2))
+    res.send(`Resultado da subtracao: ${resultado}`)
+})
+server.get('/multiplicacao', (req, res) => {
+    console.log(req.query)
+    const resultado = multiplicacao(Number(req.query.num1), Number(req.query.num2))
+    res.send(`Resultado da multiplicacao: ${resultado}`)
+})
+server.get('/divisao', (req, res) => {
+    console.log(req.query)
+    const resultado = divisao(Number(req.query.num1), Number(req.query.num2))
+    res.send(`Resultado da divisao: ${resultado}`)
+})
 
 server.listen(PORTA, () => {
     console.log(`o servidor esta online na porta ${PORTA}`)
