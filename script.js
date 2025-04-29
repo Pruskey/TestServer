9692fee33accf70bf30dd80c3c2d0a3e254d1bbc
BRUNO DAL PRÁ <bruno_pra@estudante.sesisenai.org.br>
2025-04-29 10:56:12 -0300

    bagulho de somar, dividir etc

--- script.js
+++ script.js
@@ -3,10 +3,38 @@ const http_a = require('http')
 const PORTA = 8000
 
 const server = http_a.createServer( (req, res) => {
-    console.log(req)
-    res.end('uma resposta qualquer')
+    const rota = req.url
+    if (rota == '/soma'){
+        const resultado = somar(1,2)
+    } else if (rota == '/subtrair'){
+        const resultado = subtrair(1,2)
+    } else if (rota == '/multiplicar'){
+        const resultado = multiplicar(1,2)
+    } else if (rota == '/dividir'){
+        const resultado = dividir(1,2)
+    } else {
+        const resultado = "inválido"
+    }
+    
+    res.end(`O resultado da soma é: ${resultado}`)
 } )
 
 server.listen(PORTA, () => {
     console.log(`O servidor esta rodando na porta' ${PORTA}`)
 })
+
+function somar (num1, num2){
+    return num1 + num2
+}
+
+function subtrair (num1, num2){
+    return num1 - num2
+}
+
+function multiplicar (num1, num2){
+    return num1 * num2
+}
+
+function dividir (num1, num2){
+    return num1/num2
+}
